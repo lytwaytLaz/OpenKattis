60d54316f447c5b5ef02eaf4db586b2b036ac9a3
lytwaytLaz <laszlo.hago@gmail.com>
2017-08-09 14:15:18 +0200

    Solutions to two Kattis problems

---
+++ TakeTwoStones/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace TakeTwoStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TextReader reader = Console.In)
+            {
+                Console.WriteLine(int.Parse(reader.ReadLine()) % 2 == 0 ? "Bob" : "Alice");
+            }
+        }
+    }
+}
