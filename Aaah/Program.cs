54f410179fa7a6c8197a80439c55cfeb59b2d372
lytwaytLaz <laszlo.hago@gmail.com>
2017-08-11 14:46:22 +0200

    Solving Aaah

---
+++ Aaah/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Aaah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine().Length < Console.ReadLine().Length ? "no" : "go");
+        }
+    }
+}
