5f601d03bc35e467680b67886d2ea9b5658b910d
lytwaytLaz <laszlo.hago@gmail.com>
2017-08-11 14:47:07 +0200

    Solve Peragram

---
+++ Peragram/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peragram
+{
+    class Program
+    {
+        static Dictionary<char, int> map = new Dictionary<char, int>();
+        static int counter = 0;
+
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (map.ContainsKey(str.ElementAt(i)))
+                    map[str.ElementAt(i)] = map[str.ElementAt(i)] + 1;
+                else
+                    map[str.ElementAt(i)] = 1;
+            }
+
+            foreach (int value in map.Values)
+            {
+                if (value % 2 != 0)
+                    counter += 1;
+            }
+            Console.Write(counter == 0 ? 0 : counter - 1);
+
+        }
+    }
+}
